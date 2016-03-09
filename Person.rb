d4020815d7617c784d29b01d24d2e8d168bad9dd
Benjamín Silva <benjamin@nexoit.com>
2016-03-09 19:52:53 -0300

    Commit initial..

---
+++ Person.rb
@@ -0,0 +1,29 @@
+class Person
+
+	def initialize(name, positionx, positiony)
+		@name = name
+		@positionx = positionx
+		@positiony = positiony
+	end
+
+	def run
+
+	end
+
+	def scream
+
+	end
+
+	def danger
+
+	end
+
+	def dead
+
+	end
+
+	def to_s
+
+	end
+
+end
---
+++ Zombie.rb
@@ -0,0 +1,21 @@
+class Zombie
+
+	def initialize(name, positionx, positiony)
+		@name = name
+		@positionx = positionx
+		@positiony = positiony
+	end
+
+	def walk
+
+	end
+
+	def speak
+
+	end
+
+	def to_s
+		
+	end
+
+end
